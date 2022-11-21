e382fc636c3440f9bd67a8b41758436059a835c3
Obiora Okafor <obioracelestine@gmail.com>
2022-11-21 15:37:51 +0100

    Update get_user_record.py

--- get_user_record.py
+++ get_user_record.py
@@ -13,8 +13,11 @@ class UserRecords:
         self.email_one = None
         self.email_two = None
         self.sheety_user_api = os.environ.get("SHEETY_USER_ENDPOINT")
-        self.headers = {"Authorization": "Bearer "+os.getenv("SHEETY_AUTH")}
+        self.sheety_flight = os.environ.get("SHEETY_ENDPOINT")
+        self.headers = {"Authorization": "Bearer " + os.getenv("SHEETY_AUTH")}
         self.user_dict = self.get_users()["users"]
+        self.number_of_cities = None
+        self.new_input = False
 
     def new_user(self):
         """This method registers a new user."""
@@ -30,10 +33,32 @@ class UserRecords:
                     "email": self.email_one,
                 }
             }
+            self.post_cities()
             user_response = requests.post(url=self.sheety_user_api, json=parameters, headers=self.headers)
             user_response.raise_for_status()
             print("You're in the club.")
             return user_response.json()
+
+    def post_cities(self):
+        sheety_list = requests.get(url=self.sheety_flight, headers=self.headers)
+        self.number_of_cities = int(input("How many destinations would you like to visit?\n"))
+        if self.number_of_cities > 0:
+            destination_list = [input(f"Input Destination {number+1}:\n").title()
+                                for number in range(self.number_of_cities)]
+            existing_list = []
+            for sheety_dict in sheety_list.json()["costs"]:
+                for key, value in sheety_dict.items():
+                    if key == "city":
+                        existing_list.append(value)
+            for destination in destination_list:
+                if destination not in existing_list:
+                    city_params = {
+                        "cost": {
+                            "city": destination,
+                        }
+                    }
+                    city_response = requests.post(url=self.sheety_flight, json=city_params, headers=self.headers)
+                    city_response.raise_for_status()
 
     def get_users(self):
         """This method retrieves the user records from the Google sheet."""
